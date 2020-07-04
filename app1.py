68ceb8c04cb765d14903de1327a58207b761314e
Chayan Kathuria <chayankathuria@gmail.com>
2020-07-04 19:02:20 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -24,7 +24,7 @@ from PIL import Image
 #app=Flask(__name__)
 #Swagger(app)
 
-pickle_in = open("classifier.pkl","rb")
+pickle_in = open("classifier2.pkl","rb")
 classifier=pickle.load(pickle_in)
 
 #@app.route('/')
